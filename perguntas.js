16a2372778d1304d47bf3e864b788af9702669df
matheusfobico <turossi.matheus@escola.pr.gov.br>
2025-09-24 10:40:00 -0300

    asadsad

---
+++ perguntas.js
@@ -0,0 +1,33 @@
+criaCartao(
+categoria,
+pergunta,
+resposta,
+)
+criaCartao(
+' MATEMÁTICA básica ',
+' 1 ) QUANTO É 1+1?',
+'2'
+)
+criaCartao(
+' MATEMÁTICA básica ',
+' 2 ) QUANTO É 2+2?',
+'4'
+)
+
+criaCartao(
+' MATEMÁTICA básica ',
+' 3 ) QUANTO É 4+4?',
+'8'
+)
+
+criaCartao(
+' MATEMÁTICA básica ',
+' 4 ) QUANTO É 8+8?',
+'16'
+
+)
+criaCartao(
+' MATEMÁTICA básica ',
+' 5 ) QUANTO É 16+16?',
+'32'
+)
